f1db5582cf347b79ba8ecc6eb1f80a53a2bff224
Alessandro Lamattina <lamattinalessandro@gmail.com>
2023-08-29 21:29:30 +0200

    a

--- postura.py
+++ postura.py
@@ -1,45 +1,40 @@
 import cv2
-from cv2 import cuda
+import mediapipe as mp
 
-# Crea l'oggetto VideoCapture per acquisire il video in tempo reale dalla webcam o da un file video
-cap = cv2.VideoCapture(0)  # 0 indica la webcam integrata nel computer
+# Inizializza la rilevazione della postura di Mediapipe
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
 
-# Verifica se la cattura del video è avvenuta correttamente
+# Apri lo streaming video dalla webcam
+cap = cv2.VideoCapture(0)
+
+# Verifica se la cattura è stata aperta correttamente
 if not cap.isOpened():
-    print("Errore nell'apertura del video.")
+    print("Impossibile aprire la cattura video.")
     exit()
 
-# Crea un oggetto cv2.cuda_GpuMat per leggere i frame video dalla GPU
-gpuFrame = cuda.GpuMat()
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-# Loop per leggere i frame video in tempo reale
-while True:
-    # Leggi un frame video dalla sorgente (webcam o file video)
-    ret, frame = cap.read()
+        # Converte il frame in RGB (mediapipe lavora con immagini RGB)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Verifica se il frame è stato letto correttamente
-    if not ret:
-        print("Errore nella lettura del frame.")
-        break
+        # Esegue la rilevazione della postura sul frame RGB
+        results = pose.process(rgb_frame)
 
-    # Sposta il frame sulla GPU
-    gpuFrame.upload(frame)
+        # Disegna i punti della postura sul frame originale
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
 
-    # Applica le operazioni di elaborazione desiderate utilizzando le funzioni CUDA di OpenCV
-    # Ad esempio, converti il frame a scala di grigi sulla GPU
-    gpuGray = cuda.GpuMat()
-    cuda.cvtColor(gpuFrame, cv2.COLOR_BGR2GRAY, gpuGray)
+        # Mostra il frame elaborato in tempo reale
+        cv2.imshow('Posture Detection', frame)
 
-    # Sposta il frame di output dalla GPU alla RAM
-    gray = gpuGray.download()
+        # Interrompi il ciclo quando viene premuto il tasto 'q'
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-    # Visualizza il frame elaborato
-    cv2.imshow('Frame in Scala di Grigi', gray)
-
-    # Interruzione del loop se viene premuto il tasto 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Rilascia le risorse
+# Rilascia la cattura e chiudi le finestre
 cap.release()
 cv2.destroyAllWindows()
